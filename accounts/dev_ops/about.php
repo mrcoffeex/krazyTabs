f5b55ca99d62a374a187a8d3b2607c9281e6674f
Master Kent <kentjohn.gocotano@thesiblingssolutions.com>
2022-09-04 13:43:46 +0800

    about page edits

--- accounts/dev_ops/about.php
+++ accounts/dev_ops/about.php
@@ -30,23 +30,45 @@
                                 <div class="col-md-6">
                                     <div class="card">
                                         <div class="card-body">
-                                            <h3><i class="ti-info-alt"></i> About Us</h3>
-                                            <p style="font-size: 15px; text-align: justify;">
-                                               We create/provides solutions to companies and individuals in a way of apps/softwares. Since 2017 we created several apps and helped a few companies to improved their transactions when it comes to efficiency and accuracy. Aside from the great performance and flexibility of our services. We offer Krazy affordable rates in the market.<br>
-                                               Solutions that we offer:<br>
-                                            </p>
-                                            <ul>
-                                                <li>Automated Tabulation</li>
-                                                <li>Management Systems</li>
-                                                <li>Information Systems</li>
-                                                <li>Inventory Systems</li>
-                                                <li>Online Customized Apps</li>
-                                            </ul>
-
-                                            <p class="text-primary" style="font-weight: bold;">
-                                                Email: kjohn0319@gmail.com <br>
-                                                Cel #: 0912 161 0673
-                                            </p>
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <h3 class="mb-4"><i class="ti-info-alt"></i> About Us</h3>
+                                                    <p class="mb-4" style="font-size: 15px; text-align: justify;">
+                                                    We create/provides solutions to companies and individuals in a way of apps/softwares. Since 2017 we created several apps and helped a few companies to improved their transactions when it comes to efficiency and accuracy. Aside from the great performance and flexibility of our services. We offer Krazy affordable rates in the market.
+                                                    </p>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <h5>Former/Current Clients:<br></h5>
+                                                    <ul>
+                                                        <li>Sunga Hospital (<span class="text-primary">Laboratory Results Information System</span>)</li>
+                                                        <li>SchedSave.com ((<span class="text-primary">Norway based</span>)</li>
+                                                        <li>Goncar Security Academy ((<span class="text-primary">Panabo/Tagum</span>)</li>
+                                                        <li>Little Ambassador Learning Center</li>
+                                                        <li>Taghoy Medical Clinic</li>
+                                                        <li>RMB Rymars ((<span class="text-primary">Davao</span>)</li>
+                                                        <li>ASCU Hardware</li>
+                                                        <li>HGS Hardware ((<span class="text-primary">other 3 branches</span>)</li>
+                                                    </ul>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <h5>Solutions that we offer:<br></h5>
+                                                    <ul>
+                                                        <li>Automated Tabulation</li>
+                                                        <li>Management Systems</li>
+                                                        <li>Information Systems</li>
+                                                        <li>Inventory Systems</li>
+                                                        <li>Online Customized Apps</li>
+                                                    </ul>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <p style="font-weight: bold;">
+                                                        Email:&nbsp;
+                                                        <span class="text-primary">krazyappsph@gmail.com</span>&nbsp;
+                                                        Cel #:&nbsp;
+                                                        <span class="text-primary">0912 161 0673</span>&nbsp;
+                                                    </p>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
