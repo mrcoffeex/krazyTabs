6d418788b38825bd5c0bd077632430b905513e92
mrcoffeex <kjohn0319@gmail.com>
2022-09-05 11:24:06 +0800

    live changes

--- accounts/dev_ops/judges.php
+++ accounts/dev_ops/judges.php
@@ -155,7 +155,7 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="ModalLabel"><i class="ti-plus"></i> Create User</h5>
+                    <h5 class="modal-title" id="ModalLabel"><i class="ti-plus"></i> Create Judge</h5>
                     <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                     </button>
