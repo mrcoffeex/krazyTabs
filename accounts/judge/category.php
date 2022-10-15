83a2a08ce08f0553beea763ea9c71cfcc75b7914
mrcoffeex <kjohn0319@gmail.com>
2022-10-15 10:43:45 +0800

    jquery import

--- accounts/judge/category.php
+++ accounts/judge/category.php
@@ -27,7 +27,7 @@
 <html lang="en">
 
 <?php include '_head.php'; ?>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="../../js/jquery.min.js"></script>
 
 <body>
     <div class="container-scroller">
